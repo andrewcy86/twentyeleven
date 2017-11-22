5b6cda623660ea78514ffb1552a030edfb70c569
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-22 15:16:40 -0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,7 @@ if ( 'content' != $current_layout ) :
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-				<aside id="archives" class="widget">
+				<!--<aside id="archives" class="widget">
 					<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
 					<ul>
 						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
@@ -29,7 +29,7 @@ if ( 'content' != $current_layout ) :
 						<li><?php wp_loginout(); ?></li>
 						<?php wp_meta(); ?>
 					</ul>
-				</aside>
+				</aside>-->
 
 			<?php endif; // end sidebar widget area ?>
 		</div><!-- #secondary .widget-area -->
